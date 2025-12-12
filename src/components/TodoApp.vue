52c27eaf5d61c6e668ce2e1572154a6f51053004
Ruxadev <grmdev01@gmail.com>
2025-12-12 17:14:34 -0300

    Introduce reactive state with ref

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,9 +1,17 @@
 <script setup>
+import { ref } from 'vue'
+
+const todos = ref([
+  { id: 1, text: 'Learn Vue.js', completed: false },
+  { id: 2, text: 'Build a Todo App', completed: false },
+  { id: 3, text: 'Master Reactivity', completed: false }
+])
 </script>
 
 <template>
   <div>
     <h2>This is the TodoApp Component</h2>
+    <pre>{{ todos }}</pre>
   </div>
 </template>
 
