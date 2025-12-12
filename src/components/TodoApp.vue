fdd862c33012e8953502226daa15a03e170e50e6
Ruxadev <grmdev01@gmail.com>
2025-12-12 17:27:42 -0300

    Implement user input and add todo functionality

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -6,11 +6,34 @@ const todos = ref([
   { id: 2, text: 'Build a Todo App', completed: false },
   { id: 3, text: 'Master Reactivity', completed: false }
 ])
+
+const newTodoText = ref('')
+let nextId = 4; // Simple way to generate unique IDs
+
+function addTodo() {
+  if (newTodoText.value.trim() === '') {
+    return // Don't add empty todos
+  }
+  todos.value.push({
+    id: nextId++,
+    text: newTodoText.value,
+    completed: false
+  })
+  newTodoText.value = '' // Clear the input field
+}
 </script>
 
 <template>
   <div>
     <h2>This is the TodoApp Component</h2>
+    
+    <input
+      type="text"
+      v-model="newTodoText"
+      placeholder="Add a new todo"
+    />
+    <button @click="addTodo">Add Todo</button>
+
     <pre>{{ todos }}</pre>
   </div>
 </template>
