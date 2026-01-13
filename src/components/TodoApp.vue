c9407888334c6437b5bab68ef4763eebb35eabe2
Ruxadev <grmdev01@gmail.com>
2026-01-13 16:20:36 -0300

    Integrate TodoItem and implement delete functionality

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,22 +1,28 @@
 <script setup>
 import { ref } from 'vue'
-import TodoForm from './TodoForm.vue' // 1. Import the new component
+import TodoForm from './TodoForm.vue'
+import TodoItem from './TodoItem.vue' // Import the new component
 
 const todos = ref([
   { id: 1, text: 'Learn Vue.js', completed: false },
   { id: 2, text: 'Build a Todo App', completed: false },
-  { id: 3, text: 'Master Reactivity', completed: false },
+  { id: 3, text: 'Master Reactivity', completed: false }
 ])
 
 let nextId = 4
 
-// 2. Create the event handler function
-function handleNewTodo(todoText) {
+// Renamed for clarity
+function addTodo(todoText) {
   todos.value.push({
     id: nextId++,
     text: todoText,
-    completed: false,
+    completed: false
   })
+}
+
+// Method to handle the delete event
+function deleteTodo(idToDelete) {
+  todos.value = todos.value.filter((todo) => todo.id !== idToDelete)
 }
 </script>
 
@@ -24,13 +30,15 @@ function handleNewTodo(todoText) {
   <div>
     <h2>This is the TodoApp Component</h2>
 
-    <!-- 3. Use the new component and listen for its event -->
-    <TodoForm @add-todo="handleNewTodo" />
+    <TodoForm @add-todo="addTodo" />
 
     <ul>
-      <li v-for="todo in todos" :key="todo.id">
-        {{ todo.text }}
-      </li>
+      <TodoItem
+        v-for="todo in todos"
+        :key="todo.id"
+        :todo="todo"
+        @delete-todo="deleteTodo"
+      />
     </ul>
   </div>
 </template>
