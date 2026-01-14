c91de7178e7b7db1e059ab8caaaf5d4f8d266155
Ruxadev <grmdev01@gmail.com>
2026-01-14 17:16:08 -0300

    Adapt App.vue to use Ionic mobile layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <script setup>
-import TodoApp from './components/TodoApp.vue'
+import TodoApp from './components/TodoApp.vue';
+import {
+  IonApp,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent
+} from '@ionic/vue';
 </script>
 
 <template>
-  <main>
-    <TodoApp />
-  </main>
+  <ion-app>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Vue Todo</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <TodoApp />
+    </ion-content>
+  </ion-app>
 </template>
 
-<style scoped>
-main {
-  padding: 2rem;
-  font-family: sans-serif;
-}
+<style>
+/* We can leave this empty or add global styles later */
 </style>
+
 
